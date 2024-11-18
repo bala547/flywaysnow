5b7b1f2cbf27d99ae808d66d695816f3e8033539
bala547 <balakrishnaulli3@gmail.com>
2024-11-18 19:56:55 +0530

    Create pipeline_config.groovy

---
+++ pipeline_config.groovy
@@ -0,0 +1,12 @@
+pipeline {
+    agent any
+    environment {
+        SNOWFLAKE_URL = "${SNOWFLAKE_URL}"
+        SNOWFLAKE_USER = "${SNOWFLAKE_USERNAME}"
+        SNOWFLAKE_PASSWORD = "${SNOWFLAKE_PASSWORD}"
+        FLYWAY_VERSION = "10.17.3" 
+        JAVA_TOOL_OPTIONS = "--add-opens=java.base/java.nio=ALL-UNNAMED"
+    }
+
+    template = 'mainPipelineTemplate'
+}
